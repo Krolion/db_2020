afe067e5c0eb4a3002ff9cd2be0ed9c8b602ff5c
Krolion <bendgamin7@gmail.com>
2020-09-04 16:12:03 +0300

    RadioAlarmClock2 Update

--- src/main/java/radiobudilnik/AlarmClock.java
+++ src/main/java/radiobudilnik/AlarmClock.java
@@ -2,10 +2,12 @@ package radiobudilnik;
 
 public class AlarmClock implements GeneralAlarmClock{
 
+    @Override
     public void setTime() {
         System.out.println("Time set on alarm clock");
     }
 
+    @Override
     public void stopAlarm() {
         System.out.println("PIP-PIP-pip-pip-...");
     }
--- src/main/java/radiobudilnik/Radio.java
+++ src/main/java/radiobudilnik/Radio.java
@@ -2,10 +2,12 @@ package radiobudilnik;
 
 public class Radio implements GeneralRadio{
 
+    @Override
     public void setVolume() {
         System.out.println("Volume set on radio");
     }
 
+    @Override
     public void changeChannel() {
         System.out.println("Channel changed");
     }
