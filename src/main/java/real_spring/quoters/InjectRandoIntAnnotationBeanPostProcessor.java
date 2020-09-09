5715616461a18c4d5e9ae6b7e4d512d2eae98579
Krolion <bendgamin7@gmail.com>
2020-09-09 14:37:12 +0300

    CW08_1

---
+++ src/main/java/real_spring/quoters/InjectRandoIntAnnotationBeanPostProcessor.java
@@ -0,0 +1,15 @@
+package real_spring.quoters;
+
+import my_spring.InjectRandomIntAnnotationObjectConfigurer;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class InjectRandoIntAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    private InjectRandomIntAnnotationObjectConfigurer configurer = new InjectRandomIntAnnotationObjectConfigurer
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        configurer.configure(bean, null);
+        return bean;
+    }
+
+}
