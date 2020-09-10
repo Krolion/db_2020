6325721533404d58ca810d253443f365cbe0e4e8
Krolion <bendgamin7@gmail.com>
2020-09-10 10:25:04 +0300

    HW

---
+++ src/main/java/real_spring/quoters/Config.java
@@ -0,0 +1,11 @@
+package real_spring.quoters;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("real_spring")
+public class Config {
+}
--- src/main/java/real_spring/quoters/Main.java
+++ src/main/java/real_spring/quoters/Main.java
@@ -1,5 +1,6 @@
 package real_spring.quoters;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -7,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 
         context.close();
     }
