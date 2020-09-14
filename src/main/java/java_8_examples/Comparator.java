9d3ba947464517caaedeb5d310f529e5c1a463a1
Krolion <bendgamin7@gmail.com>
2020-09-14 10:58:48 +0300

    Equals

---
+++ src/main/java/java_8_examples/Comparator.java
@@ -0,0 +1,5 @@
+package java_8_examples;
+
+public interface Comparator<T> {
+    boolean equal(T t1, T t2);
+}
--- src/main/java/java_8_examples/ListUtils.java
+++ src/main/java/java_8_examples/ListUtils.java
@@ -6,10 +6,10 @@ import java.util.List;
  * @author Evgeny Borisov
  */
 public class ListUtils {
-    public static <T> int countDuplicates(List<T> list, T t) {
+    public static <T> int countDuplicates(List<T> list, T t, Comparator<T> comparator) {
         int counter=0;
         for (T t1 : list) {
-            if (t1.equals(t)) {
+            if (comparator.equal(t1, t)) {
                 counter++;
             }
         }
