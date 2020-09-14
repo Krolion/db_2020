4723268b1ebfacada54ee93893bdc32533118b32
Krolion <bendgamin7@gmail.com>
2020-09-14 10:50:15 +0300

    trash

--- src/main/java/real_spring/quoters/Config.java
+++ src/main/java/real_spring/quoters/Config.java
@@ -3,9 +3,11 @@ package real_spring.quoters;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan("real_spring")
+@PropertySource("classpath:application.properties")
 public class Config {
 }
